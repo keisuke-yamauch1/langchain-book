121e02296ea78014b0f39c6530134976c2a2f4de
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-17 14:46:09 +0900

    Agents終わり

--- app/main.py
+++ app/main.py
@@ -12,10 +12,8 @@ def get_chat():
 
 if __name__ == "__main__":
     chat = get_chat()
-    tools = load_tools(["terminal"])
-    agent_chain = initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+    tools = load_tools(["ddg-search"])
+    agent_chain = initialize_agent(tools, chat, agent=AgentType.OPENAI_MULTI_FUNCTIONS)
 
-    result = agent_chain.run("sample_data ディレクトリにあるファイルの一覧を教えて")
+    result = agent_chain.run("東京と大阪の天気を教えて")
     print(result)
-
-
